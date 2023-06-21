8f3bc966b88947ef3e06f3a7a8b6ff7fac92cc81
kshitiz <kshitiz123goel@gmail.com>
2023-06-21 23:20:27 +0530

    Palindrome code added

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -6,8 +6,42 @@
   - `npm run test-palindrome`
 */
 
+function reverse(str){
+  let reverse = "";
+  for(let i=str.length-1;i>=0;i--){
+    if(str[i] ===  " " || str[i] ===  "," || str[i] ===  "!" || str[i] ===  "?"|| str[i] === "."){
+
+    }
+    else{
+    reverse = reverse + str[i];
+  }
+}
+
+return reverse;
+}
+
 function isPalindrome(str) {
+ 
+  str =  str.toLowerCase();
+
+  let orignal = "";
+  for(let i=0;i<str.length;i++){
+    if(str[i] ===  " " || str[i] ===  "," || str[i] ===  "!" || str[i] ===  "?" || str[i] === "."){
+
+    }
+    else{
+      orignal = orignal + str[i];
+    }
+  }
+
+  
+  
+  if(reverse(str) == orignal){
   return true;
+}
+  else{
+    return false;
+  }
 }
 
 module.exports = isPalindrome;
