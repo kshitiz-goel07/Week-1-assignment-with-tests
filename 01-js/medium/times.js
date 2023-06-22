a67ee67e34c6888dc807a1aaa46ff03064300243
kshitiz <kshitiz123goel@gmail.com>
2023-06-22 22:23:47 +0530

    used Date function

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,17 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = new Date().getTime();
+    var sum = 0;
+    for ( let i=0;i<n;i++){
+        sum = sum + i;
+    }
+    let endTime = new Date().getTime();
+
+    console.log((startTime-endTime)/1000);
+
+    return startTime - endTime;
+    // return 0.01;
 }
+
+calculateTime(1000000000);
